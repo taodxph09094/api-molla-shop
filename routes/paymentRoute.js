f3d57d5f6c25f965f43bbf2370901455f38b62f5
taodx09094 <taodxph09094@gmail.com>
2023-08-12 19:55:30 +0700

    .

--- routes/paymentRoute.js
+++ routes/paymentRoute.js
@@ -6,8 +6,12 @@ const {
 const router = express.Router();
 const { isAuthenticatedUser } = require("../middleware/auth");
 
-router.route("/payment/process").post(isAuthenticatedUser, processPayment);
+// router.route("/payment/process").post(isAuthenticatedUser, processPayment);
 
-router.route("/stripeapikey").get(isAuthenticatedUser, sendStripeApiKey);
+// router.route("/stripeapikey").get(isAuthenticatedUser, sendStripeApiKey);
+
+router.route("/payment/process").post(processPayment);
+
+router.route("/stripeapikey").get(sendStripeApiKey);
 
 module.exports = router;
